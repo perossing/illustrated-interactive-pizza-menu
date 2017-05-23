a76a7287d9bc7c7e3b2e658bc940713f8da01aaf
Peter Rossing <perossing@gmail.com>
2017-05-22 17:30:08 -0700

    flowchart in PPT, JS logs selections grouped by menu section to console (menu sections now id's in HTML)

--- pizza-menu.js
+++ pizza-menu.js
@@ -1,11 +1,37 @@
-
-
-// functions referenced in HTML onclick captureEvents
-
 /*
-
+// functions referenced in HTML onclick captureEvents:
 showCharges()
-
 resetMenu()
+confirmOrder()
+*/
 
-*.
+const pricing = {
+    personal: 6,
+    medium: 10,
+    large: 14,
+    xLarge: 20,
+    topping: 1,
+    premium: 3
+};
+
+
+const menuSections = document.querySelectorAll('form > section');
+
+
+let getCheckedItems = () => {
+    for (let i = 0; i < menuSections.length; i++) {
+
+        // insert this into HTML...
+        console.log(menuSections[i].id);
+
+        let menuOptions = menuSections[i].querySelectorAll('input');
+
+        for (let j = 0; j < menuOptions.length; j++) {
+            if (menuOptions[j].checked) {
+
+                //insert these into HTML....
+                console.log('     ' + menuOptions[j].value);
+            }
+        }
+    }
+};
